1de95ebcc7853ac2c6f44870e7de9177a45d045a
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-18 08:56:31 +0000

    datatypes

---
+++ js-chaiorcode/ch03-datatypes.js
@@ -0,0 +1,128 @@
+"use strict"  // treat all js code as newer version es6
+console.log("hello vikas lets do this u ready")
+
+// alert("hey bot")  
+// alert will output a dialog box or alret box in our browser on the top of the document ... 
+
+// ehh nibba no popup arrived
+// its not js browser engine its not we cant do this here u can do this in brower console ok...
+
+// spacing
+
+// we avoid semicolons while in react 
+// try to make your code readable
+
+// console.log("vikas"),console.log("hey") // its a bad practice noreadibily accepted 
+
+console.log("vikas");
+console.log("hey");
+
+// this is the good habbit
+
+// now lets dicsuss about datatypes in js
+
+// what are datatypes ?
+ 
+// se when ever we want to do some operations on our variables like console.log(10+vikas) computer did.nt understand how to solve this as computer will take 10+vikas = 10vikas so to solve this we have to give a datatype according to the value a variable holds
+
+console.log(10+"vikas"); 
+ // we will see we have aoutput saying 10vikas but is it ok to add a number with some characters no it is not..... also what the hell is number and this characters means ...?
+
+ // in js simply we have 8 type of datatypes means we can store 8 types of data in js.... 
+
+
+
+ //number
+ //string
+ //boolean
+ //undefined
+ //null
+ //object
+ //symbols
+ //big int
+
+
+
+ //confused...?
+ 
+ //let me state more supose we have a empty vessel and we can store anytype of thing in it right.... ummm lets say my fav is balabjamun i will fill my empty vessel with balamjalam..... but my mind changes after sometime and i drop that balamjalam and fill my empty vessel with a metal gold... woww....
+
+ // means we have a container or vessel whihc is called as a varibale in js and we can store anytype of value insdie them like charatcters or nu,bers or etc..
+
+ // lets find how many dataypes we have 
+
+ // we have total 8 data types
+
+ let age = 18;   // here the name of the empty vessel is age and we store a number value in it .. and it should be a number as our logic says... 
+
+ //what youo want to store a name here .... o gosh come on ok lets do this..
+
+ age = "potato"  // yes it will run fine but your age is pottao as it doesnt make snese..... 
+
+ let name = "vikas";  // here we name that empty vessel name and value this container storing is a combination of characters vikas.... \
+ // remember we add a double quote and then inside vikas.... these double quotes tells computer that name vessel has a value called STRING 
+
+ //what is string....?
+
+ // simply a string is a datatype in which we store a combination of character or even a single character it twlls computer that these are not maths number 
+
+ // if we take "2" +"2"  // this will output 22 as inside a double quote it is treated a s astring datatype and maths oprations are perfomed like a fool...
+
+ // but why 22 why not 4 ..?
+ // see when we want to add m=number for god sake letts say...what we do vikas +boy=vikasboy.. we take characters and ad them means combined them in next to eaach other no maths operations....
+ // and whn we do 2+2 we get 4 as we know in maths 2+2 is 4 but if "3+3"
+//  it will output 3+3 as now thesee numbers are trated a s a character abd character always adds next to them not add but combined even it willnot tolarte if + is added it takes + as  a character too "3"+"3"  33
+
+let userActive=true;
+
+// what is this true....?
+// dont worry its just a data type means it only has two values true or false means ya to hai ya nhi hai...
+
+// true hold 1 false hold 0 
+
+// eh nigga stop what u mean 1  and 0...
+// means true has value 1 and false says 0 
+
+console.log(true+true);
+// it will return us 2 as 1+1 is 2 
+
+let distanceFromSunToBlackolen = BigInt("12345678987654323456789876543211234567898765432");
+ console.log(distanceFromSunToBlackolen);
+
+ // bigint is used to store a big number as the name specifys big integer..
+
+ // the  term bigint tells that the vessel or varibale is a bigint 
+
+ let big=BigInt("2326376473674637"); // inside double quotes only numbers allowed not chararcyters..
+
+ console.log(big); 
+
+ let dog; // it is a undefined datatype as the vessel named dog has no value but it can add some value so it is undefined yet...
+
+ let cat=null;  // now by writing null we state that veese or variable cat has no value and it will neither holds a value... alos type of null is a object
+
+ let kite = Symbol("yellow");
+
+ let kite2=Symbol("yellow");
+
+ console.log(kite)
+ console.log(kite2);
+
+
+ // a symbol is a datatype which help us to make our variable unique although they can have a same value... 
+
+ let car={
+    name:"alto",
+    price:200000
+ };
+
+ // a object is  adataype where we can store our data with key value pairs or with properties and values like car datatype may have properties like name or price etc ..
+
+ console.log(car.price);
+ // here we say that from car we need value of price property
+
+ // so thats about datatypes... bye seee u sooon
+
+
+
+ 
