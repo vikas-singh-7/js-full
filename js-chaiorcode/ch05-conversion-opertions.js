b2e19fbac423ba048ef3926641fcf3aa014f6cb3
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-18 15:23:28 +0000

    conversions and operations

---
+++ js-chaiorcode/ch05-conversion-opertions.js
@@ -0,0 +1,56 @@
+let value = 4;
+
+console.log(value);  // 4 
+
+// lets try to change this value into negative number
+
+let negValue = -value;  // if we wamt to chnage a number variable into a negative numbe variable we just put a - before its name
+
+console.log(negValue);  // output -4
+
+console.log(2+2); // output 4 
+console.log(2-2); // output 0
+console.log(2*2); // output 4 
+console.log(8%3) // output 2 as modulus shows us the reminder 
+console.log(2**3) // output 8 as it says 2 power 3 means 2*2*2
+
+console.log(3/3) // output 1 as 3 divide by 3 will be 1 
+
+
+console.log("33" + 33);
+
+// return us 3333 as string + number 
+
+console.log("33" +33+33);
+
+// return us 333333 as string is in the first place so it will concat not add
+  console.log(33+33+"33");
+
+  // return us 6633 as first two numbers will add and then "33" will concat 
+
+
+  let num1,num2,num3;  // we declared many variables in a same line...
+  
+  
+  num1 = num2 =num3 =4 ;  // assign a same value to the variables num1 = num2 =num3 = 4 means all the varibles are eqal and have value 4 
+
+  console.log(num1+num2+num3);  // retuns 12 as 4+4+4 = 12... 
+
+
+
+  console.table([num1,num2,num3]);
+
+  console.log(true);  // returns true
+
+  console.log(+true);  // retuns 1
+  
+  console.log(true+true);  // returnd 2 as true holds value 1 
+
+  console.log(1+true+true+false)  // we are saying 1+1+1+0 which is 3
+
+  // as false hold 0 value
+
+
+
+
+
