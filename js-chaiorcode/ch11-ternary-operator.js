bd9f34666342c93eecf1a76b0b2116eb17f09f3a
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-19 09:20:02 +0000

    THE TERNARY OPERATOR

---
+++ js-chaiorcode/ch11-ternary-operator.js
@@ -0,0 +1,28 @@
+let num = 40;
+
+(num==40)?console.log("hello 40"):console.log("condition get fails num is not 40")
+
+//
+
+//another exapmple 
+//  
+
+let age = 17;
+
+let algble;
+
+(age>=18 && age<=50)? algble="yes you are eligible for driving":algble="no yoou are not eligible nigga"
+
+// or we can directly assign the value to algble 
+
+algble=(age>=18 && age<=50)?"yes you are eligible for driving":"no yoou are not eligible nigga"
+
+
+
+
+console.log(algble);
+
+
+// simply terny opertaor is a if else but it can only run ne statement after the ondition getstrue or false 
+
+// syntax is      var a =  (condition)?true:fasle    var a holds the value of the result of the ternary
