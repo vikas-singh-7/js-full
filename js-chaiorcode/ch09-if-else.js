a65ef11651963fb4475b584d9ff3a50e174fdf5d
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-19 06:32:56 +0000

    if-else

---
+++ js-chaiorcode/ch09-if-else.js
@@ -0,0 +1,16 @@
+let a = 11;
+
+if(a==10){
+    console.log("hey ")
+}else{
+    console.log("bye")
+};
+
+let gender = "male";
+let name = "vikas"
+
+if (gender == "male"){
+    console.log("hello mr." )
+}else{ 
+     console.log("helllo miss ")
+};
