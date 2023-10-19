6c5f9edcbf9f98db350f0c2ea8720d0283e90cb0
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-19 05:33:08 +0000

    comparison

---
+++ js-chaiorcode/ch06-omparison.js
@@ -0,0 +1,43 @@
+// lets leran comparison opwrtaorss 
+// comaprison oprators alwatys returns us true or false
+// how many typs of comparison operators are 
+ let a = 10; // this is  anumbr 
+
+ let b = "10"; // this is a string but valuaes are same
+
+ console.log(a==b);  // it will retur true as the vaue of both a and b is sam as 10 
+
+ console.log(a===b);  // RETURN FALSE
+  // hre we state that value and datattype of a is eqa;l to b whih is not true as b is holding the samee value thoughbhut the datattype of b is a string
+
+  console.log(true==1); // will return true as ture holds 1 value and `1 is one so both arfe eqaul
+
+  console.log(true===1); // false as true is holding the samew value though but the datatype  of true is a boolean and datattypoe of 1 is a number 
+
+  // > < is greater then and is less then tehse are alo vomparison operators 
+
+  let  c = 10;
+  let d = 11;
+
+  console.log(d>c);  // retuns true as d is grerater then c 
+
+  console.log(d<c); // returns false as d is not sa=maller and we satte thgat d is damlller then c 
+
+  // !=   is not equals !== is not eqal and dattaype too
+
+  let e = 10;
+  let f = 11;
+
+  console.log(e!=f); // retur true as e is not eqaul to f 
+
+console.log(true !=1); // reyurns false as true is eqL TO 1 
+
+console.log(true!== 1); // returns true as ture and 1 is not eqal in dattype
+
+
+
+// thanks thats all abt comparison operators :) 
+
+
+
+
