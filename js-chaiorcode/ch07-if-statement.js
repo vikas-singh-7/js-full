936b7db457e32cbb5c3cb901ca150901390b2a6b
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-19 05:47:58 +0000

    if-statement

---
+++ js-chaiorcode/ch07-if-statement.js
@@ -0,0 +1,31 @@
+let a = 10;
+let b =11;
+
+if(a<b){
+    console.log("hello vikas a is less than b ");
+
+};
+
+let c =1;
+if(c==true){
+    console.log("yes c is one and true is also one")
+}
+
+// if( c === true){
+//     console.log("no the dattaypes are not equal altjough the vaues arfe dsame ")
+// }  as the conditon get false as c is not stricty;y eqals to true as true is a boolan and c is a number so it is a false conditioj and if doesnt work on false condition
+
+let name = "vikas";
+let srname="singh";
+
+if(name == srname){
+    console.log("hello")  // it wll not run as the value of name and srname aere df 
+}
+
+let fname="vikas";
+let lname = "vikas";
+
+if(fname===lname){
+    console.log("hey vikas");   // it will print vikas as fname has the same vale as ;name so theb condition gets true and we get the statement
+}
+
