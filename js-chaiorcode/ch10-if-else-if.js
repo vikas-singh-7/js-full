673b1c2f55ecea1b32d05cdb9c2900f0b1f0ce45
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-19 08:59:46 +0000

    IF_ELSE_IF

---
+++ js-chaiorcode/ch10-if-else-if.js
@@ -0,0 +1,23 @@
+// lets learn if else if
+
+// we know we have a condition inside a if and if the contiion gets true the code insde if eavluate and if the condition of the if is not true thne outomatically code of else works but
+// now we want to check many condition inside a if so we neeed if else if ,means if a condition gets fails check oother if conditrion and after that chek other if condirio iif that if fails too
+
+
+let marks =74;
+
+if(marks>=90 && marks <=100){
+    console.log("wow dog u got A+")
+}else if(marks>=75 && marks <=89){
+    console.log(" great you got the A")
+
+}else if(marks>=60 && marks<=74){
+    console.log("good keep doing you will do good  B+")
+}else if(marks>=45 && marks <=59){
+    console.log("ypu are passed but need to work more ")
+}else{
+    console.log("TRY AGAIN")
+};
+
+
+// simply if else eif tells us that if one condiiton got fails try another and if that other cindition gets fails too try another ..... 
