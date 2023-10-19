30c0eb83068277b90660365a1ab6095028b08788
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-19 10:04:05 +0000

    SWITCH_CASE

---
+++ js-chaiorcode/ch12-switch-case.js
@@ -0,0 +1,29 @@
+let day = 1;
+
+switch (true) {
+  case day == 1:
+    console.log("hey monday");
+    break;
+  case day == 2:
+    console.log("tuesday");
+    break;
+  case day == 3:
+    console.log("wednesday");
+    break;
+  case day == 4:
+    console.log("thursday");
+    break;
+  case day == 5:
+    console.log("friday");
+    break;
+  case day == 6:
+    console.log("saturday");
+    break;
+  case day == 7:
+    console.log("sunday");
+    break;
+  default:
+    console.log("enter a valid week day");
+}
+
+// it is similar to if else if in switch we take a expression and make caase for expersions case diff value
