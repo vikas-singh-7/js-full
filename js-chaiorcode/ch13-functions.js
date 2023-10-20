42125b6bcbba6c4bd6575b7a1606e25796503ba5
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-20 06:28:45 +0000

    function with return

---
+++ js-chaiorcode/ch13-functions.js
@@ -0,0 +1,15 @@
+function sum (eng,hindi,sci){
+    return  eng+hindi+sci;
+};
+
+ var tt = sum (45,66,78);
+
+ console.log(tt);
+
+ function per (total){
+    return total/300*100;
+
+
+ }
+ var percentage = per(tt);
+ console.log(percentage);
