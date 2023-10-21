597254f35111a4502ae216f58e8ce500d447d0c1
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-21 06:36:52 +0000

    push and pop

---
+++ js-chaiorcode/17 arraypush pop.js
@@ -0,0 +1,32 @@
+// if we need to remove the very last valueb of the array we are not given th elenght and index of values then how we cant delete the last value....
+
+// we can do this by pop()function
+
+var arr=[1,2,3,4,5,6,7,8,9,0];
+console.log(arr);
+
+arr.pop();  // remove the very last value of the array
+
+
+console.log(arr);
+
+// remember pop is used when we didnt know the index of the last value
+
+// array.push();
+
+// if somesome says add a new value to thevery last index of the array but u  have not given the index so we need  push function
+
+var arr1=["vikas","vishal","Rohit","rakesh","rishu"];
+
+console.log(arr1);
+
+// now add lucky to last index and the last index is not given so
+
+arr1.push("lucky") // adds lukcy value to last index 
+
+console.log(arr1);
+
+
+// push adds a new bvalue at the last index 
+// pop removes the last value of an array
+
