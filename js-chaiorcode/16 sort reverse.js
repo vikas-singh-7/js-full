7b98189a73a3f76f484e2156e0a82ac5d41df03e
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-21 06:19:35 +0000

    array.sort array.reverse

---
+++ js-chaiorcode/16 sort reverse.js
@@ -0,0 +1,27 @@
+// in js when we want to make our array alphabetically  ordered we use sort method... sort makes our array values aligned as alphabetically 
+
+var arr =["vikas","rohit","abu","rakesh","sandeep","nirmal"];
+
+console.log(arr);
+
+// now allignes the valuesnin alphabeticsally
+
+arr.sort(); // makes the values acc to alphabeticaly
+
+ console.log(arr);
+
+
+ // lets see how to make the values in an array reverse means the value on 0index swil b eon last index and so on ;
+
+ var rev=[1,2,3,4,5,6,7,8,9];
+
+ console.log(rev);
+
+ // noe to reverse the values of array
+
+ rev.reverse(); // will make the values reverse
+ //
+ console.log(rev)
+
+ // so array.sort amd array.reverse functions help to array to make its value if alphabeticaly aligned then array.sort  if reversly alligned then array.reverse
+ 
