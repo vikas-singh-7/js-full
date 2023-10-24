888955fa7985125ffa82a480bfc787b0c6ff7312
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-24 06:53:40 +0000

    forInLoop

---
+++ js-chaiorcode/33 for in loop.js
@@ -0,0 +1,15 @@
+var obj ={
+    name:"vikas",
+    age:22,
+    lname:"singh"
+};
+
+for(var key in obj){
+    console.log(key + " " + obj[key]);
+
+}
+
+// here in for in loop we use for(var key in object) 
+// means the baribale key is going to add the propertyes of obj
+
+// obj[property/name] = value of object
