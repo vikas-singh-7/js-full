d4a7165ff2017e7f512ff50924c76ba3166ff208
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-24 15:48:20 +0000

    string.TRIM

---
+++ js-chaiorcode/38 trim.js
@@ -0,0 +1,9 @@
+// simply i w ehahv a lot of space in starting or ening in an string the unwanting space we can remove it using trim
+
+var str="     hey nigga    ";   // space in staringa nd end
+
+console.log(str);
+
+var a = str.trim();   // removes the extra or unwanting space in the end or beginnening of a string 
+
+console.log(a)
