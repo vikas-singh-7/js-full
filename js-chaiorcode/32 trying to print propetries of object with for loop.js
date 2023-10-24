775fcbb52815698c61d30e54f1d80519df894a57
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-24 06:34:57 +0000

    object with for loop

---
+++ js-chaiorcode/32 trying to print propetries of object with for loop.js
@@ -0,0 +1,16 @@
+  var obj = {
+    name:"vikas",
+    lname:"singh",
+    age:22,
+    girlfriend:"no"
+  };
+
+  for(var a=0 ; a<=3; a++){
+    console.log(obj[a])
+  };  //
+
+  //// so we cant print object propertyiesm with a for loop ye we try w]but get failed
+
+  
+
+  
