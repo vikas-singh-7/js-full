af9169d67bd80c7a214cc9d9bfd42945cb75d65e
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-25 05:50:35 +0000

    toString() valuepf()

---
+++ js-chaiorcode/42 tostring valueof.js
@@ -0,0 +1,19 @@
+// tostring mtho hlp to mak any numri or any othr atatyp into a string 
+
+var a = 100;
+
+// a is  anumbr 
+
+var b = a.toString();
+console.log(b);
+
+console.log(b.charAt(2));  // string mthods will work here as the 100 is now a string 
+
+// valueof () function sim[ly tells us about the vaue of string like the print
+
+var str1="vikas";
+var m = str1.valueOf();   // same as print 
+console.log(m);
+
+
+
