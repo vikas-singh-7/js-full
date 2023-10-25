a17b85d9b5f2e1a3d98f6720a753429468674cc9
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-25 14:12:08 +0000

    toFixed-toPrecesion

---
+++ js-chaiorcode/44 tofixed toprcesion.js
@@ -0,0 +1,14 @@
+// to fixd is usd whene w hav a long imal numbr and we want to makee it a fixed decimal son we use to fixed 
+
+var a = 10.873;
+
+var b = a.toFixed(2);   // 10.87   as we say after decial only take two values remove the rest
+
+console.log(b);   
+
+// toPecision()   // it is used  to roundoff
+
+var c = 10.782;
+
+var h = c.toPrecision(2); //  11 as 
+console.log(h)
