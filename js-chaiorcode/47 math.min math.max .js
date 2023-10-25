7ab75e69f0526966cc61bbc55724f21935746d4e
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-25 15:41:56 +0000

    math.min - math.max

---
+++ js-chaiorcode/47 math.min math.max .js
@@ -0,0 +1,17 @@
+// if wee want to see min umber in an niumber we use math.min
+
+var a = Math.min(10,27);
+console.log(a);
+
+
+var b = [10,23,343,23,45,73,23,11,23,13,42,34,1];
+
+var d = Math.min(...b);   // we use spread o[eratpr and we spread the array into each individual values and we use math.min]
+
+console.log(d)
+
+// for max value 
+
+var m = Math.max(...b);
+console.log(m)
+
