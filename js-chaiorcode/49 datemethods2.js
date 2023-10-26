3e98fb90b88fc46b18a970efe6c969bc4cb818ed
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-26 04:51:22 +0000

    dateMethods

---
+++ js-chaiorcode/49 datemethods2.js
@@ -0,0 +1,28 @@
+// date methods 2 
+// make a object of date first and then apply methods
+
+var now = new Date();
+console.log(now.getHours())   // return the hour 
+
+console.log(now.getMinutes());  // returns minutes
+
+// we can also know the day of a particular day like
+
+var gugu = new Date("may 24 2005");
+
+console.log(gugu.getDay());  // so gugu was born on tuesday
+
+var vikas = new Date("january 6 2001");
+console.log(vikas.getDate());
+console.log(vikas.getDay()); // 6 so vikas was born on saturday
+
+// these were dategetmethods
+// getday();
+// getdate();
+// gethours();
+// getMinutes();
+// getmilliseocnd();
+// getyear();
+
+
+
