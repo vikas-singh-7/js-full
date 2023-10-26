731db3f328527b9d0f58dbdec11a1c706c3d0795
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-26 04:42:04 +0000

    DAteMethods

---
+++ js-chaiorcode/48 datemethods().js
@@ -0,0 +1,34 @@
+/// date methods are used to get date and time 
+// for using date metods we have to make the object of the date
+// we have to make a variable a object of date
+
+var now = new Date();
+
+console.log(now.toDateString());  // toDateString return u the date in readable form like day month year
+
+// getdate;   // as name suggest that it will return us the date not the month or year
+
+
+console.log(now.getDate())    // ddate withjout month and uyear
+
+
+// getFullYear()    // it will return u =sthe full year without date or day
+
+console.log(now.getFullYear())  // only year like 2023
+
+// getMonth() as name suggest it will return us the current month
+
+console.log(now.getMonth());   // in numeric way but ir returns month like oct == 9 
+
+// getday()  as the name sugegest it will get us the curent day 
+
+console.log(now.getDay());  // returns in numeric like its thurdsay and it will sgow 4 
+
+// in this we see date methods 
+
+// first we have ro make a object of date and then on rhat object we can apply ethjds
+
+// getday getdate getyear getmont
+//todatestring  // full date in dd mm yy
+
+  
