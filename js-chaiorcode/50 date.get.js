c11a0aa7ebe738071b10b0546995f0e1536a083c
vikas <147738566+vikas-singh-7@users.noreply.github.com>
2023-10-26 05:08:10 +0000

    SETDATE()

---
+++ js-chaiorcode/50 date.get.js
@@ -0,0 +1,9 @@
+// lets learn setdate 
+
+/// setdate methods useed to set the date or hours or mins to the current date
+
+var now = new Date();
+
+now.setMonth(1);   // this set the current month to one means feb as 0 == january
+console.log(now);
+
